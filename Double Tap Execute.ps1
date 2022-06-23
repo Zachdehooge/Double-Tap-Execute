0156becc82d6cd1635e65717da5bfb77009bec8c
Zach DeHooge <47896974+Zachdehooge@users.noreply.github.com>
2022-06-23 15:32:50 -0400

    Update Double Tap Execute.ps1

--- Double Tap Execute.ps1
+++ Double Tap Execute.ps1
@@ -3,8 +3,6 @@ If (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]:
     Exit
 }
 
-Write-Host "Changing Powershell to Execute on Click..."
-
 New-PSDrive -PSProvider registry -Root HKEY_CLASSES_ROOT -Name HKCR
 
 Set-ItemProperty -Path "HKCR:\Microsoft.PowerShellScript.1\Shell\Open\Command" -Name "(Default)" -Type String -Value "`"C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe`" -noLogo -ExecutionPolicy unrestricted -file `"%1`""
